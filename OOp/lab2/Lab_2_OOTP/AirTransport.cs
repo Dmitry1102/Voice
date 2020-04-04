128a5b7985bce449f586156fad8aa1b6444b883b
Дмитрий <61946119+Dmitry1102@users.noreply.github.com>
2020-04-04 21:00:40 +0300

    Add files via upload

---
+++ OOp/lab2/Lab_2_OOTP/AirTransport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2_OOTP
+{
+    class AirTransport:Transport
+    {
+        public int Capacity
+        {
+            get; set;
+        }
+        public int Weight
+        {
+            get; set;
+        }
+        public AirTransport( string name, int cost, Factory fact, Driver driver, string engine, int capacity, int weight) : base(name, cost, fact, driver, engine)
+        {
+            Capacity = capacity;
+            Weight = weight;
+        }
+
+    }
+}
---
+++ OOp/lab2/Lab_2_OOTP/GroundTransport.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2_OOTP
+{
+    class GroundTransport: Transport
+    {
+        public int Transmission
+        {
+            get; set;
+        }
+        
+        public GroundTransport(  string name, int cost, Factory fact, Driver driver, string engine, int transmission): base(name, cost, fact,  driver,  engine)
+        { 
+            Transmission = transmission;
+            
+        }
+    }
+}
